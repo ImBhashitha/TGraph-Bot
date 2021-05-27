8032aa302bdcd12a8793512dee2a942d7075bc8b
BHASHITHA <Pramudith.bhashitha17@gmail.com>
2021-05-27 13:46:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config(object):
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     API_ID = int(os.getenv("API_ID"))
     API_HASH = os.getenv("API_HASH")
-    UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL")
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL")
